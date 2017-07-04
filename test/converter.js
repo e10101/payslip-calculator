6b7a35b3ba22aae84cc83c4c6f96e4034897a42e
Yishi <baicaibang@gmail.com>
2017-07-05 02:07:25 +1200

    Added converter test

---
+++ test/converter.js
@@ -0,0 +1,62 @@
+var expect = require('chai').expect;
+var conv = require('../lib/converter');
+
+describe('Converter', function() {
+    describe('#convert', function() {
+        it('shoud convert object', function() {
+            const obj = {
+                annualSalary: 60050,
+                superRate: 9,
+                firstName: 'David',
+                lastName: 'Rudd',
+                paymentStartDate: '01 March – 31 March',
+            };
+            const target = {
+                name: 'David Rudd',
+                payPeriod: '01 March – 31 March',
+                grossIncome: 5004,
+                incomeTax: 922,
+                netIncome: 4082,
+                superMoney: 450
+            };
+            const converted = conv.convert(obj);
+            expect(converted).to.eql(target);
+        });
+
+        it('shoud work with 9%', function() {
+            const obj = {
+                annualSalary: 60050,
+                superRate: '9%',
+                firstName: 'David',
+                lastName: 'Rudd',
+                paymentStartDate: '01 March – 31 March',
+            };
+            const target = {
+                name: 'David Rudd',
+                payPeriod: '01 March – 31 March',
+                grossIncome: 5004,
+                incomeTax: 922,
+                netIncome: 4082,
+                superMoney: 450
+            };
+            const converted = conv.convert(obj);
+            expect(converted).to.eql(target);
+        });
+    });
+
+    describe('#stringify', function() {
+        it('shoud work', function() {
+            const obj = {
+                name: 'David Rudd',
+                payPeriod: '01 March – 31 March',
+                grossIncome: 5004,
+                incomeTax: 922,
+                netIncome: 4082,
+                superMoney: 450
+            };
+            const target = 'David Rudd,01 March – 31 March,5004,922,4082,450\n';
+            const str = conv.stringify(obj);
+            expect(str).to.eql(target);
+        });
+    });
+});
