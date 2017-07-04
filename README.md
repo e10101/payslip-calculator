55410fe733eb1ef43b5402849b4d6562cc4cee4b
Yishi <baicaibang@gmail.com>
2017-07-04 23:32:31 +1200

    Added command argv and output results to file

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # payslip-calculator
 
-## Run
+## Setup
 ```
-node calculator.js
+npm install
+```
+
+## Usage
+```
+Usage: node ./index.js --input [file]
+
+Options:
+  --input, -i   The input csv file.                                [required]
+  --rates, -r   The tax rates json file.                           [default: "tax_rates.json"]
+  --output, -o  The output csv file, default output command line.
+```
+
+## Demo
+```
+node index.js --input sample.csv --output output.csv
 ```
---
+++ index.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
+var payslip = require('./lib/converter');
+var path = require('path');
+var argv = require('optimist')
+    .usage('Usage: $0 --input [file]')
+    .alias('input', 'i')
+    .alias('output', 'o')
+    .alias('rates', 'r')
+    .describe('input', 'The input csv file.')
+    .describe('rates', 'The tax rates json file.')
+    .describe('output', 'The output csv file, default output command line.')
+    .default('rates', 'tax_rates.json')
+    .demand(['input'])
+    .argv;
+
+// Get the input file path
+var inputFile = path.resolve(__dirname, argv.input);
+// Get the tax rates file path
+var taxRatesFile = path.resolve(__dirname, argv.rates);
+
+// Default output to terminal
+var output = process.stdout;
+
+// If set the output params, save the output to file.
+if (argv.output) {
+    output = fs.createWriteStream(path.resolve(__dirname, argv.output));
+}
+
+// Set the tax rates
+payslip.setTaxRates(taxRatesFile);
+
+// Read from input file
+var input = fs.createReadStream(inputFile);
+
+// Set csv file columns
+var parser = parse({columns: ['firstName', 'lastName', 'annualSalary', 'superRate', 'paymentStartDate']});
+
+// Transform the input to output
+var transformer = transform(function(record, callback) {
+    // Calculate the employee monthly payslip
+    var converted = payslip.convert(record);
+
+    // Send the result to stream
+    return callback(null, payslip.stringify(converted));
+});
+
+// By Using the stream, convert input to output
+input.pipe(parser)
+    .pipe(transformer)
+    .pipe(output);
